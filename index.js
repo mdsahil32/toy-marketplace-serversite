dd69f333ccc81ed9b9639ac30d2e89315b763b66
mdsahil32 <mohammedsahil3211@gmail.com>
2023-05-19 03:39:28 +0600

    create to get and get oneid

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express()
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 const category = require('./CategoryData/Data.json')
@@ -30,7 +30,18 @@ async function run() {
 
     await client.connect();
 
-    
+    app.get('/addToy', async(req, res) =>{
+        const cursor= toyCollection.find()
+        const result = await cursor.toArray()
+        res.send(result)
+    })
+
+    app.get('/addToy/:id', async(req, res) =>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await toyCollection.findOne(query)
+        res.send(result)
+    })
 
     app.post('/addToy', async(req, res) =>{
         const addToys = req.body;
